f2ca540ede196f0db9263a322ab7c701abd5e758
jthomson04 <jwillthomson19@gmail.com>
2026-03-20 13:44:59 -0700

    emit events for L2 prefetch
    Signed-off-by: jthomson04 <jwillthomson19@gmail.com>

--- python/sglang/srt/mem_cache/hiradix_cache.py
+++ python/sglang/srt/mem_cache/hiradix_cache.py
@@ -1404,6 +1404,9 @@ class HiRadixCache(RadixCache):
             self._update_host_leaf_status(new_node)
             self._update_leaf_status(node)
             self._update_host_leaf_status(node)
+            # Publish the newly materialized host suffix immediately so downstream
+            # cache indexers can resolve descendants that extend this L2-only prefix.
+            self._record_store_event(new_node)
 
         return matched_length
 
